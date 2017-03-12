c528f07e93dff5f97e32e205aabcfc82a4d7ae6e
pst <ruslan.ermeichook@gmail.com>
2017-03-12 09:12:24 +0300

    Start project. First commit

---
+++ Ado.Net/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado.Net
+{
+    class Program
+    {
+
+        public Program()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Ado.net!");
+        }
+    }
+}
